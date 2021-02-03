df3306f6ca5da0541bbf08f2940a54700434bcaf
Balinder Walia <bwalia@tenthmatrix.co.uk>
2021-02-03 01:14:09 +0100

    Yaml templates added correct /kube/kubectl path

--- lfc-bo-nodejs/lfc-bo-create-deployment-service.sh
+++ lfc-bo-nodejs/lfc-bo-create-deployment-service.sh
@@ -5,29 +5,29 @@
 
 #Make sure minio is always running
 
-#/usr/local/bin/kubectl delete -f kube/minio.yaml
-/usr/local/bin/kubectl apply -f kube/minio.yaml
+# /kube/kubectl delete -f kube/minio.yaml
+ /kube/kubectl apply -f kube/minio.yaml
 
-#sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" create-service-nginx-template.yml | /usr/local/bin/kubectl delete -f -
-#sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/lfc-bo.yaml | /usr/local/bin/kubectl delete -f -
-#sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/mongo.yaml | /usr/local/bin/kubectl delete -f -
+#sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" create-service-nginx-template.yml |  /kube/kubectl delete -f -
+#sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/lfc-bo.yaml |  /kube/kubectl delete -f -
+#sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/mongo.yaml |  /kube/kubectl delete -f -
 
 #sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/minio.yaml > kube/minio-$1.yaml
 
 sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/lfc-bo.yaml > kube/lfc-bo-$1.yaml
 sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/mongo.yaml > kube/mongo-$1.yaml
 
-/usr/local/bin/kubectl apply -f kube/lfc-bo-$1.yaml
-/usr/local/bin/kubectl apply -f kube/mongo-$1.yaml
+ /kube/kubectl apply -f kube/lfc-bo-$1.yaml
+ /kube/kubectl apply -f kube/mongo-$1.yaml
 
-#sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/lfc-bo.yaml | /usr/local/bin/kubectl apply -f -
-#sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/mongo.yaml | /usr/local/bin/kubectl apply -f -
+#sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/lfc-bo.yaml |  /kube/kubectl apply -f -
+#sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/mongo.yaml |  /kube/kubectl apply -f -
 
 sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/lfc-rpxy-nginx.yml > kube/lfc-rpxy-nginx-$1.yaml
 
-/usr/local/bin/kubectl apply -f kube/lfc-rpxy-nginx-$1.yaml
+ /kube/kubectl apply -f kube/lfc-rpxy-nginx-$1.yaml
 
-##/usr/local/bin/kubectl get deployments --all-namespaces
+## /kube/kubectl get deployments --all-namespaces
 
-##/usr/local/bin/kubectl get svc --all-namespaces
+## /kube/kubectl get svc --all-namespaces
 
