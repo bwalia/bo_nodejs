9eab9510bcf77fb697ae63f31000e2aa9f68b0ed
Balinder Walia <bwalia@tenthmatrix.co.uk>
2021-02-03 03:01:46 +0100

    BO Node JS App version release v

---
+++ lfc-bo-nodejs/release-bo-app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#echo $1
+#echo $2
+#APP_VERSION#
+
+sed "s^#APP_VERSION#^$1^g" views/input-template.pug > views/input.pug
+
+docker build . -t bo-nodejs:latest
+
+docker tag bo-nodejs:latest images.mobiguider.cloud/bo-nodejs
+
+docker push images.mobiguider.cloud/bo-nodejs
+
+git add *
+
+git commit -m "BO Node JS App version release v $1"
+
+git push
+
+
