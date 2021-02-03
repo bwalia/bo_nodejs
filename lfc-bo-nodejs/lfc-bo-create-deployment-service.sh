3d40aa2cfefa1f2517f85968f563567fb87c555b
Balinder Walia <bwalia@tenthmatrix.co.uk>
2021-02-03 03:15:33 +0100

    BO Node JS App version release v 1.0.7 build 0003

--- lfc-bo-nodejs/lfc-bo-create-deployment-service.sh
+++ lfc-bo-nodejs/lfc-bo-create-deployment-service.sh
@@ -6,7 +6,7 @@
 #Make sure minio is always running
 
 # /kube/kubectl delete -f kube/minio.yaml
- /kube/kubectl apply -f kube/minio.yaml
+/kube/kubectl apply -f kube/minio.yaml
 
 #sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" create-service-nginx-template.yml |  /kube/kubectl delete -f -
 #sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/lfc-bo.yaml |  /kube/kubectl delete -f -
@@ -17,17 +17,18 @@
 sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/lfc-bo.yaml > kube/lfc-bo-$1.yaml
 sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/mongo.yaml > kube/mongo-$1.yaml
 
- /kube/kubectl apply -f kube/lfc-bo-$1.yaml
- /kube/kubectl apply -f kube/mongo-$1.yaml
+/kube/kubectl delete -f kube/lfc-bo-$1.yaml
+
+/kube/kubectl apply -f kube/lfc-bo-$1.yaml
+/kube/kubectl apply -f kube/mongo-$1.yaml
 
 #sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/lfc-bo.yaml |  /kube/kubectl apply -f -
 #sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/mongo.yaml |  /kube/kubectl apply -f -
 
 sed "s^#SERVICE_CODE#^$1^g;s^#SERVICE_PORT#^$2^g" kube/lfc-rpxy-nginx.yml > kube/lfc-rpxy-nginx-$1.yaml
 
- /kube/kubectl apply -f kube/lfc-rpxy-nginx-$1.yaml
+/kube/kubectl apply -f kube/lfc-rpxy-nginx-$1.yaml
 
 ## /kube/kubectl get deployments --all-namespaces
-
 ## /kube/kubectl get svc --all-namespaces
 
